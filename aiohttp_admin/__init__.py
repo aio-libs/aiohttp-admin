058f64c447de4b8392a52bf1b7fdb4e0a83b58b4
Nickolai Novik <is.infinity@yahoo.com>
2016-01-03 16:54:29 +0200

    now we can provide folter for additional admin templates

--- aiohttp_admin/__init__.py
+++ aiohttp_admin/__init__.py
@@ -1,7 +1,6 @@
 import aiohttp_jinja2
 import jinja2
 
-
 from .admin import Admin
 from .base import BaseView
 from .consts import APP_KEY, TEMPLATE_APP_KEY
@@ -14,13 +13,25 @@ __version__ = '0.0.1'
 
 def setup(app, name=None, url=None, index_view=None,
           translations_path=None, endpoint=None, static_url_path=None,
-          base_template=None, template_mode=None, category_icon_classes=None):
+          base_template=None, template_mode='bootstrap3',
+          category_icon_classes=None,
+          template_folder=None):
 
     app.router.add_static('/static/admin',
                           path=str(PROJ_ROOT / 'static'),
                           name='admin.static')
 
-    loader = jinja2.FileSystemLoader([str(TEMPLATES_ROOT)])
+    # gather template folders: default and provided
+    if not isinstance(template_folder, list):
+        template_folder = [template_folder]
+    template_root = str(TEMPLATES_ROOT / template_mode)
+    if template_folder is None:
+        template_folders = [template_root]
+    else:
+        template_folders = [template_root] + template_folder
+
+    # init aiohttp_jinja plugin
+    loader = jinja2.FileSystemLoader(template_folders)
     aiohttp_jinja2.setup(app, loader=loader, app_key=TEMPLATE_APP_KEY)
 
     admin = Admin(app, name=name, url=url, index_view=index_view,
