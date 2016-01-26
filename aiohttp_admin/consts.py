1a9701e7aff214e0434655063644597f4ae8951c
Nickolai Novik <is.infinity@yahoo.com>
2016-01-26 22:26:23 +0200

    remove old files

--- aiohttp_admin/consts.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-
-
-PROJ_ROOT = pathlib.Path(__file__).parent.resolve()
-TEMPLATES_ROOT = PROJ_ROOT / 'templates'
-APP_KEY = 'aiohttp_admin'
-TEMPLATE_APP_KEY = 'aiohttp_admin.templates'
