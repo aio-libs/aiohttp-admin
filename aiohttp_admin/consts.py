6d4f2ea6ddf8a0ce729579fe035fb55d6e4165d7
Nickolai Novik <is.infinity@yahoo.com>
2016-02-15 00:41:04 +0200

    and consta module

---
+++ aiohttp_admin/consts.py
@@ -0,0 +1,7 @@
+import pathlib
+
+
+APP_KEY = 'aiohttp_admin'
+TEMPLATES_ROOT = pathlib.Path(__file__).parent / 'templates'
+PROJ_ROOT = pathlib.Path(__file__).parent
+TEMPLATE_APP_KEY = 'aiohttp_admin_templates'
