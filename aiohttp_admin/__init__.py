6d6c2604e7d8e5e6625b35f8ce7190195242090e
Nickolai Novik <is.infinity@yahoo.com>
2016-02-15 00:41:51 +0200

    redo admin setup

--- aiohttp_admin/__init__.py
+++ aiohttp_admin/__init__.py
@@ -1,19 +1,13 @@
-import pathlib
 import aiohttp_jinja2
 import jinja2
 
 
-from .admin import Admin
+from .admin import Admin, index_handler
+from .consts import TEMPLATES_ROOT, PROJ_ROOT, TEMPLATE_APP_KEY, APP_KEY
 
 
 __all__ = ['Admin', 'setup']
 __version__ = '0.0.1'
-
-
-APP_KEY = 'aiohttp_admin'
-TEMPLATES_ROOT = pathlib.Path(__file__).parent / 'templates'
-PROJ_ROOT = pathlib.Path(__file__).parent
-TEMPLATE_APP_KEY = 'aiohttp_admin_templates'
 
 
 def gather_tempalte_folder(template_folder):
@@ -28,16 +22,24 @@ def gather_tempalte_folder(template_folder):
     return template_folders
 
 
-def setup(app, url=None, static_url_path=None, template_folder=None):
+def setup(app, admin_conf_path, url=None, static_url=None,
+          template_folder=None):
     loop = app.loop
-    url = url or 'admin'
-    static_url_path = static_url_path or '/admin/static'
-    app.router.add_static(static_url_path,
+
+    # setup routes
+    url = url or '/admin'
+    static_url = static_url or '/admin/static'
+
+    app.router.add_route('GET', url, index_handler, name='admin.index')
+    app.router.add_static(static_url,
                           path=str(PROJ_ROOT / 'static'),
                           name='admin.static')
-    tf = gather_tempalte_folder(template_folder)
+    app.router.add_static('/admin/config',
+                          path=admin_conf_path,
+                          name='admin.config')
 
     # init aiohttp_jinja plugin
+    tf = gather_tempalte_folder(template_folder)
     loader = jinja2.FileSystemLoader(tf)
     aiohttp_jinja2.setup(app, loader=loader, app_key=TEMPLATE_APP_KEY)
 
