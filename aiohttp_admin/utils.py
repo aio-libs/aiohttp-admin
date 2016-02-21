445650240ca1f145eb93b9166107a581c0ebc276
Nickolai Novik <is.infinity@yahoo.com>
2016-02-21 14:50:14 +0200

    add moved utils

--- aiohttp_admin/utils.py
+++ aiohttp_admin/utils.py
@@ -5,8 +5,11 @@ from datetime import datetime, date
 import trafaret as t
 from aiohttp import web
 
+from .exceptions import JsonValidaitonError
+from .consts import TEMPLATES_ROOT
 
-__all__ = ['json_response']
+
+__all__ = ['json_response', 'validate_query', 'gather_template_folders']
 
 
 def json_datetime_serial(obj):
@@ -29,9 +32,12 @@ OptKey = partial(t.Key, optional=True)
 Filter = t.Dict({
     OptKey('in'): t.List(t.String),
     OptKey('gt'): t.String,
-    OptKey('gte'): t.String,
+    OptKey('ge'): t.String,
     OptKey('lt'): t.String,
-    OptKey('lte'): t.String,
+    OptKey('le'): t.String,
+    OptKey('ne'): t.String,
+    OptKey('eq'): t.String,
+    OptKey('like'): t.String,
 })
 
 SimpleType = t.Int | t.Bool | t.String | t.Float
@@ -46,13 +52,30 @@ ListQuery = t.Dict({
 
 def validate_query(query):
     query_dict = dict(query)
-    filters = query.get('_filters')
-    if filters is not None:
+    filters = query_dict.pop('_filters', None)
+    if filters:
         try:
             f = json.loads(filters)
         except ValueError as e:
             raise t.DataError('_filters can not be serialised') from e
         else:
             query_dict['_filters'] = f
-    q = ListQuery(query_dict)
+
+    try:
+        q = ListQuery(query_dict)
+    except t.DataError as exc:
+        raise JsonValidaitonError(**exc.as_dict())
+
     return q
+
+
+def gather_template_folders(template_folder):
+    # gather template folders: default and provided
+    if not isinstance(template_folder, list):
+        template_folder = [template_folder]
+    template_root = str(TEMPLATES_ROOT)
+    if template_folder is None:
+        template_folders = [template_root]
+    else:
+        template_folders = [template_root] + template_folder
+    return template_folders
