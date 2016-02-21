59f8dccd9b438abb0043cd282526de0374ca2de0
Nickolai Novik <is.infinity@yahoo.com>
2016-02-21 14:49:45 +0200

    refacto and expose get_admin

--- aiohttp_admin/__init__.py
+++ aiohttp_admin/__init__.py
@@ -2,35 +2,28 @@ import aiohttp_jinja2
 import jinja2
 
 
-from .admin import Admin, index_handler
-from .consts import TEMPLATES_ROOT, PROJ_ROOT, TEMPLATE_APP_KEY, APP_KEY
+from .admin import Admin, get_admin
+from .consts import PROJ_ROOT, TEMPLATE_APP_KEY, APP_KEY
+from .utils import gather_template_folders
 
 
-__all__ = ['Admin', 'setup']
+__all__ = ['Admin', 'setup', 'get_admin']
 __version__ = '0.0.1'
 
 
-def gather_tempalte_folder(template_folder):
-    # gather template folders: default and provided
-    if not isinstance(template_folder, list):
-        template_folder = [template_folder]
-    template_root = str(TEMPLATES_ROOT)
-    if template_folder is None:
-        template_folders = [template_root]
-    else:
-        template_folders = [template_root] + template_folder
-    return template_folders
-
-
 def setup(app, admin_conf_path, url=None, static_url=None,
-          template_folder=None):
+          template_folder=None, name=None, app_key=APP_KEY):
     loop = app.loop
+
+    admin = Admin(app, url=url, name=name, loop=loop)
+    # add support for multiple admins sites
+    app[APP_KEY] = admin
 
     # setup routes
     url = url or '/admin'
     static_url = static_url or '/admin/static'
 
-    app.router.add_route('GET', url, index_handler, name='admin.index')
+    app.router.add_route('GET', url, admin.index_handler, name='admin.index')
     app.router.add_static(static_url,
                           path=str(PROJ_ROOT / 'static'),
                           name='admin.static')
@@ -39,11 +32,8 @@ def setup(app, admin_conf_path, url=None, static_url=None,
                           name='admin.config')
 
     # init aiohttp_jinja plugin
-    tf = gather_tempalte_folder(template_folder)
+    tf = gather_template_folders(template_folder)
     loader = jinja2.FileSystemLoader(tf)
     aiohttp_jinja2.setup(app, loader=loader, app_key=TEMPLATE_APP_KEY)
 
-    admin = Admin(app, url=url, loop=loop)
-    # add support for multiple admins sites
-    app[APP_KEY] = admin
     return admin
