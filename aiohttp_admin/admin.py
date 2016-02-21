089b9fb3433710b25deff9b2288b9020aa041765
Nickolai Novik <is.infinity@yahoo.com>
2016-02-21 14:49:04 +0200

    move handler to admin class

--- aiohttp_admin/admin.py
+++ aiohttp_admin/admin.py
@@ -1,5 +1,6 @@
 import aiohttp_jinja2
-from .consts import TEMPLATE_APP_KEY
+
+from .consts import TEMPLATE_APP_KEY, APP_KEY
 
 
 __all__ = ['Admin', 'admin_middleware_factory']
@@ -16,23 +17,31 @@ async def admin_middleware_factory(app, handler):
     return admin_middleware
 
 
-@aiohttp_jinja2.template('admin.html', app_key=TEMPLATE_APP_KEY)
-async def index_handler(request):
-    return {}
+def get_admin(app, *, app_key=APP_KEY):
+    return app.get(app_key)
 
 
 class Admin:
 
-    def __init__(self, app, *, url=None, loop):
+    def __init__(self, app, *, name=None, url=None, loop):
         self._app = app
         self._loop = loop
         self._resources = []
-        self._url = url or 'admin'
+        self._url = url or '/admin'
+        self._name = name or 'aiohttp_admin'
 
     @property
     def app(self):
         return self._app
 
+    @property
+    def name(self):
+        return self._name
+
     def add_resource(self, resource):
         resource.setup(self.app, self._url)
         self._resources.append(resource)
+
+    @aiohttp_jinja2.template('admin.html', app_key=TEMPLATE_APP_KEY)
+    async def index_handler(self, request):
+        return {'name': self._name}
