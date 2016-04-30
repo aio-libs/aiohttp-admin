d144c20db870c57655462de01492ada6659be467
Nickolai Novik <nickolai@datarobot.com>
2016-04-30 10:54:27 +0300

    add mysql fixture

--- tests/docker_fixtures.py
+++ tests/docker_fixtures.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import psycopg2
+import pymysql
 import pytest
 
 from docker import Client as DockerClient
@@ -70,6 +71,58 @@ def pg_server(unused_port, docker, session_id, docker_pull):
         pytest.fail("Cannot start postgres server")
     container['port'] = port
     container['pg_params'] = pg_params
+    yield container
+
+    docker.kill(container=container['Id'])
+    docker.remove_container(container['Id'])
+
+
+@pytest.fixture
+def mysql_params(mysql_server):
+    return dict(**mysql_server['mysql_params'])
+
+
+@pytest.yield_fixture(scope='session')
+def mysql_server(unused_port, docker, session_id, docker_pull):
+    mysql_tag = '5.7'
+    if docker_pull:
+        docker.pull('mysql:{}'.format(mysql_tag))
+    port = unused_port()
+    container = docker.create_container(
+        image='mysql:{}'.format(mysql_tag),
+        name='mysql-test-server-{}-{}'.format(mysql_tag, session_id),
+        ports=[3306],
+        detach=True,
+        environment={'MYSQL_USER': 'aiohttp_admin',
+                     'MYSQL_PASSWORD': 'mysecretpassword',
+                     'MYSQL_DATABASE': 'aiohttp_admin',
+                     'MYSQL_ROOT_PASSWORD': 'mysecretpassword'},
+        host_config=docker.create_host_config(port_bindings={3306: port})
+    )
+    docker.start(container=container['Id'])
+    host = os.environ.get('DOCKER_MACHINE_IP', '127.0.0.1')
+    mysql_params = dict(database='aiohttp_admin',
+                        user='aiohttp_admin',
+                        password='mysecretpassword',
+                        host=host,
+                        port=port)
+    delay = 0.001
+    for i in range(100):
+        try:
+            conn = pymysql.connect(**mysql_params)
+            cur = conn.cursor()
+            cur.execute("SELECT 1;")
+            cur.close()
+            conn.close()
+            break
+        except pymysql.Error as e:
+            print(e)
+            time.sleep(delay)
+            delay *= 2
+    else:
+        pytest.fail("Cannot start postgres server: {}".format(e))
+    container['port'] = port
+    container['mysql_params'] = mysql_params
     yield container
 
     docker.kill(container=container['Id'])
