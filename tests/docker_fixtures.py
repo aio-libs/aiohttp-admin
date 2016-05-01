9368e70e1130bd2a5129834236e45ef2210a7baa
Nickolai Novik <nickolai@datarobot.com>
2016-05-01 12:15:19 +0300

    refactor pg fixture

--- tests/docker_fixtures.py
+++ tests/docker_fixtures.py
@@ -65,47 +65,45 @@ def container_starter(request, docker, session_id, docker_pull):
     return f
 
 
-@pytest.yield_fixture(scope='session')
-def pg_server(unused_port, docker, session_id, docker_pull):
-    pg_tag = "9.5"
-    print("Pulling postgress image")
-    if docker_pull:
-        docker.pull('postgres:{}'.format(pg_tag))
-    port = unused_port()
-    container = docker.create_container(
-        image='postgres:{}'.format(pg_tag),
-        name='aiopg-test-server-{}-{}'.format(pg_tag, session_id),
-        ports=[5432],
-        detach=True,
-        host_config=docker.create_host_config(port_bindings={5432: port})
-    )
-    docker.start(container=container['Id'])
-    host = os.environ.get('DOCKER_MACHINE_IP', '127.0.0.1')
-    pg_params = dict(database='postgres',
-                     user='postgres',
-                     password='mysecretpassword',
-                     host=host,
-                     port=port)
+def wait_for_container(callable, image, skip_exception):
     delay = 0.001
     for i in range(100):
         try:
-            conn = psycopg2.connect(**pg_params)
-            cur = conn.cursor()
-            cur.close()
-            conn.close()
+            callable()
             break
-        except psycopg2.Error as e:
-            print("Retry n = {} after e: {}".format(i, e))
+        except skip_exception:
             time.sleep(delay)
             delay *= 2
     else:
-        pytest.fail("Cannot start postgres server")
-    container['port'] = port
-    container['pg_params'] = pg_params
-    yield container
+        pytest.fail("Cannot start {} server".format(image))
 
-    docker.kill(container=container['Id'])
-    docker.remove_container(container['Id'])
+
+@pytest.yield_fixture(scope='session')
+def pg_server(unused_port, container_starter, docker_pull):
+    tag = "9.5"
+    image = 'postgres:{}'.format(tag)
+
+    internal_port = 5432
+    host_port = unused_port()
+
+    container = container_starter(image, internal_port, host_port)
+
+    host = os.environ.get('DOCKER_MACHINE_IP', '127.0.0.1')
+    params = dict(database='postgres',
+                  user='postgres',
+                  password='mysecretpassword',
+                  host=host,
+                  port=host_port)
+
+    def connect():
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.close()
+        conn.close()
+
+    wait_for_container(connect, image, psycopg2.Error)
+    container['params'] = params
+    yield container
 
 
 @pytest.fixture
