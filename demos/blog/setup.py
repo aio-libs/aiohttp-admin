899307a2c36354a88d7429fb486fe7119c41c381
Nickolai Novik <nickolainovik@gmail.com>
2016-05-09 21:15:17 +0300

    add setup py for blog demo

---
+++ demos/blog/setup.py
@@ -0,0 +1,36 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'aiohttpdemo_blog', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            msg = 'Cannot find version in aiohttpdemo_blog/__init__.py'
+            raise RuntimeError(msg)
+
+
+install_requires = ['aiohttp',
+                    'aiohttp_admin',
+                    'trafaret',
+                    'aiohttp_jinja2',
+                    'pyyaml',
+                    'aiopg']
+
+
+setup(name='aiohttpdemo_blog',
+      version=read_version(),
+      description='Blog project example from aiohttp_admin',
+      platforms=['POSIX'],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=install_requires,
+      zip_safe=False)
