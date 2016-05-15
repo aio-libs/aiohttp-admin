98f716b0e2ce1368a8d42bcbb4170e5d9508a4e5
Nickolai Novik <nickolainovik@gmail.com>
2016-05-15 22:20:31 +0300

    add more motortwit files

---
+++ demos/motortwit/motortwit/security.py
@@ -0,0 +1,15 @@
+import bcrypt
+import base64
+
+
+def generate_password_hash(password, salt_rounds=12):
+    password_bin = password.encode('utf-8')
+    hashed = bcrypt.hashpw(password_bin, bcrypt.gensalt(salt_rounds))
+    encoded = base64.b64encode(hashed)
+    return encoded.decode('utf-8')
+
+
+def check_password_hash(encoded, password):
+    hashed = base64.b64decode(encoded)
+    is_correct = bcrypt.hashpw(password, hashed) == hashed
+    return is_correct
---
+++ demos/motortwit/motortwit/utils.py
@@ -0,0 +1,38 @@
+import pytz
+import yaml
+from hashlib import md5
+from dateutil.parser import parse
+
+import motor.motor_asyncio as aiomotor
+
+
+def load_config(fname):
+    with open(fname, 'rt') as f:
+        data = yaml.load(f)
+    # TODO: add config validation
+    return data
+
+
+async def init_mongo(conf, loop):
+    mongo_uri = "mongodb://{}:{}".format(conf['host'], conf['port'])
+    conn = aiomotor.AsyncIOMotorClient(
+        mongo_uri,
+        max_pool_size=conf['max_pool_size'],
+        io_loop=loop)
+    await conn.open()
+    db_name = conf['database']
+    return conn[db_name]
+
+
+def robo_avatar_url(email, size=80):
+    """Return the gravatar image for the given email address."""
+    hash = md5(email.strip().lower().encode('utf-8')).hexdigest()
+    url = "https://robohash.org/{hash}.png?size={size}x{size}".format(
+        hash=hash, size=size)
+    return url
+
+
+def format_datetime(timestamp):
+    if isinstance(timestamp, str):
+        timestamp = parse(timestamp)
+    return timestamp.replace(tzinfo=pytz.utc).strftime('%Y-%m-%d @ %H:%M')
