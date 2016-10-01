646421ea39389d20fc90ab3776a7a0b7303ed069
Nickolai Novik <nickolainovik@gmail.com>
2016-10-02 00:21:01 +0300

    refactor admin handlers, add token relate routes

--- aiohttp_admin/__init__.py
+++ aiohttp_admin/__init__.py
@@ -3,7 +3,7 @@ import jinja2
 from yarl import URL
 
 
-from .admin import Admin, get_admin
+from .admin import Admin, get_admin, setup_admin_handlers
 from .consts import PROJ_ROOT, TEMPLATE_APP_KEY, APP_KEY
 from .utils import gather_template_folders
 
@@ -15,25 +15,23 @@ __version__ = '0.0.1'
 def setup(app, admin_conf_path, *, url=None, static_url=None,
           static_folder=None, template_folder=None, template_name=None,
           name=None, app_key=APP_KEY):
-    loop = app.loop
-    template_name = template_name or 'admin.html'
-    admin = Admin(app, url=url, name=name, template=template_name, loop=loop)
-    # add support for multiple admins sites
-    app[app_key] = admin
-
-    # setup routes
-    url = URL(url or '/admin')
-    static_url = static_url or '/admin/static'
-    static_folder = static_folder or str(PROJ_ROOT / 'static')
-
-    r = app.router
-    r.add_route('GET', str(url), admin.index_page, name='admin.index')
-    r.add_static(static_url, path=static_folder, name='admin.static')
-    r.add_static('/admin/config', path=admin_conf_path, name='admin.config')
 
     # init aiohttp_jinja plugin
     tf = gather_template_folders(template_folder)
     loader = jinja2.FileSystemLoader(tf)
     aiohttp_jinja2.setup(app, loader=loader, app_key=TEMPLATE_APP_KEY)
 
+    template_name = template_name or 'admin.html'
+    admin = Admin(app, url=url, name=name, template=template_name,
+                  loop=app.loop)
+
+    # TODO: add support for multiple admins sites
+    app[app_key] = admin
+
+    # setup admin routes
+    url = URL(url or '/admin')
+    static_url = static_url or '/admin/static'
+    static_folder = static_folder or str(PROJ_ROOT / 'static')
+    setup_admin_handlers(admin, url, static_url, static_folder,
+                         admin_conf_path)
     return admin
--- aiohttp_admin/admin.py
+++ aiohttp_admin/admin.py
@@ -1,7 +1,10 @@
 from aiohttp_jinja2 import render_template
+from aiohttp_security import remember, forget
 from yarl import URL
 
 from .consts import TEMPLATE_APP_KEY, APP_KEY
+from .security import authorize
+from .utils import json_response, validate_payload, LoginForm, redirect
 
 
 __all__ = ['Admin', 'get_admin']
@@ -21,6 +24,7 @@ class Admin:
         self._url = URL(url or '/admin')
         self._name = name or 'aiohttp_admin'
         self._temalate = template or 'admin.html'
+        self._login_template = 'login.html'
 
     @property
     def app(self):
@@ -42,3 +46,38 @@ class Admin:
         t = self._temalate
         context = {'name': self._name}
         return render_template(t, request, context, app_key=TEMPLATE_APP_KEY)
+
+    async def login_page(self, request):
+        t = self._login_template
+        context = {}
+        return render_template(t, request, context, app_key=TEMPLATE_APP_KEY)
+
+    async def token(self, request):
+        raw_payload = await request.read()
+        data = validate_payload(raw_payload, LoginForm)
+        await authorize(request, data['username'], data['password'])
+
+        router = request.app.router
+        location = router["admin.index"].url()
+        payload = {"location": location}
+        response = json_response(payload)
+        await remember(request, response, data['username'])
+        return response
+
+    async def logout(self, request):
+        response = redirect("admin.login")
+        await forget(request, response)
+        return response
+
+
+def setup_admin_handlers(admin, url, static_url, static_folder,
+                         admin_conf_path):
+    add_route = admin.app.router.add_route
+    add_static = admin.app.router.add_static
+    a = admin
+    add_route('GET', str(url), a.index_page, name='admin.index')
+    add_route('GET', str(url / 'login'), a.login_page, name='admin.login')
+    add_route('POST', str(url / 'token'), a.token, name='admin.token')
+    add_route('DELETE', str(url / 'logout'), a.logout, name='admin.logout')
+    add_static(static_url, path=static_folder, name='admin.static')
+    add_static('/admin/config', path=admin_conf_path, name='admin.config')
