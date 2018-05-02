9ab2015d520a1705e74ae643ccb6d8c512962299
misha.gavela <Arfey17.mg@gmail.com>
2018-05-03 01:22:08 +0300

    feat: fixed show page and added simple docs for `ReadMe.md`

--- aiohttp_admin/static/react-admin/js/components/Edit/Edit.jsx
+++ aiohttp_admin/static/react-admin/js/components/Edit/Edit.jsx
@@ -2,37 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
-    Datagrid,
-    List,
-    TextField,
-    DateField,
-    NumberField,
-    BooleanField,
-    FunctionField,
-    TabbedForm,
-    FormTab,
-    TextInput,
-    DateInput,
-    ReferenceManyField,
-    NbItemsField,
-    EditButton,
-    ProductReferenceField,
-    StarRatingField,
-    SegmentsInput,
-    NullableBooleanInput,
-    Edit
+  Datagrid,
+  List,
+  TextField,
+  DateField,
+  NumberField,
+  BooleanField,
+  FunctionField,
+  TabbedForm,
+  FormTab,
+  TextInput,
+  DateInput,
+  ReferenceManyField,
+  NbItemsField,
+  EditButton,
+  ProductReferenceField,
+  StarRatingField,
+  SegmentsInput,
+  NullableBooleanInput,
+  Edit,
+  SimpleForm
 } from 'admin-on-rest';
 
 
 export const BaseEdit = (props) => (
-    <Edit title="Title" {...props}>
-        <TabbedForm>
-            <FormTab label="resources.customers.tabs.identity">
-                <TextInput source="id" style={{ display: 'block' }} />
-                <TextInput source="title" style={{ display: 'block', width: 'auto' }} />
-                <TextInput source="name" style={{ display: 'block', width: 'auto' }} />
-                <TextInput source="PICTURES" style={{ display: 'block', width: 'auto' }} />
-            </FormTab>
-        </TabbedForm>
-    </Edit>
+  <Edit {...props}>
+      <SimpleForm>
+        <TextInput source="id" style={{ display: 'block' }} />
+      </ SimpleForm>
+  </Edit>
 );
--- aiohttp_admin/static/react-admin/js/components/Show/Show.jsx
+++ aiohttp_admin/static/react-admin/js/components/Show/Show.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
-    TextInput,
-    Show,
-    SimpleForm,
-    LongTextInput,
+  Show,
+  TextField,
+  SimpleShowLayout
 } from 'admin-on-rest';
 
 
 export const BaseShow = (props) => (
-    <Show {...props}>
-        <SimpleForm>
-            <TextInput source="title" />
-            <LongTextInput source="body" />
-        </SimpleForm>
-    </Show>
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+    </SimpleShowLayout>
+  </Show>
 );
