9af52443143caa637aa1ca09f0b3182e1d8cc12c
Sam Bull <aa6bs0@sambull.org>
2023-02-16 19:49:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from distutils.command.sdist import sdist as sdist_orig
 from pathlib import Path
 from setuptools import setup, find_packages
 
@@ -29,8 +30,16 @@ classifiers = (
 )
 
 
+class sdist(sdist_orig):
+    def run(self):
+        self.spawn(["yarn", "install"])
+        self.spawn(["yarn", "build"])
+        super().run()
+
+
 setup(name="aiohttp-admin",
       version=read_version(),
+      cmdclass={"sdist": sdist}
       description="admin interface for aiohttp application",
       long_description="\n\n".join((Path("README.rst").read_text(), Path("CHANGES.rst").read_text())),
       classifiers=classifiers,
