ddfb7664eee1e4e2ef6d56c8155ba11586f46801
Sam Bull <git@sambull.org>
2023-04-22 16:24:15 +0100

    Add tests

---
+++ tests/_resources.py
@@ -0,0 +1,40 @@
+from typing import Union
+
+from aiohttp_admin.backends.abc import (
+    AbstractAdminResource, CreateParams, DeleteManyParams, DeleteParams, GetListParams,
+    GetManyParams, GetOneParams, UpdateManyParams, UpdateParams, Record)
+from aiohttp_admin.types import FieldState, InputState
+
+
+class DummyResource(AbstractAdminResource):
+    def __init__(self, name: str, fields: dict[str, FieldState],
+                 inputs: dict[str, InputState], repr_field: str):
+        self.name = name
+        self.fields = fields
+        self.inputs = inputs
+        self.repr_field = repr_field
+        super().__init__()
+
+    async def get_list(self, params: GetListParams) -> tuple[list[Record], int]:
+        return ([], 0)
+
+    async def get_one(self, params: GetOneParams) -> Record:
+        return {}
+
+    async def get_many(self, params: GetManyParams) -> list[Record]:
+        return []
+
+    async def update(self, params: UpdateParams) -> Record:
+        return {}
+
+    async def update_many(self, params: UpdateManyParams) -> list[Union[int, str]]:
+        return []
+
+    async def create(self, params: CreateParams) -> Record:
+        return {}
+
+    async def delete(self, params: DeleteParams) -> Record:
+        return {}
+
+    async def delete_many(self, params: DeleteManyParams) -> list[Union[int, str]]:
+        return []
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,7 +1,9 @@
 from aiohttp import web
+import pytest
 
 import aiohttp_admin
 from _auth import check_credentials
+from _resources import DummyResource
 
 
 def test_path() -> None:
@@ -15,3 +17,26 @@ def test_path() -> None:
     admin = aiohttp_admin.setup(app, schema, path="/another/admin")
 
     assert str(admin.router["index"].url_for()) == "/another/admin"
+
+
+def test_display() -> None:
+    app = web.Application()
+    model = DummyResource("test", {"id": {"type": "TextField", "props": {}},
+                                   "foo": {"type": "TextField", "props": {}}}, {}, "id")
+    schema: aiohttp_admin.Schema = {"security": {"check_credentials": check_credentials},
+                                    "resources": ({"model": model, "display": ("foo",)},)}
+
+    admin = aiohttp_admin.setup(app, schema)
+
+    assert admin["state"]["resources"]["test"]["list_omit"] == ("id",)
+
+
+def test_display_invalid() -> None:
+    app = web.Application()
+    model = DummyResource("test", {"id": {"type": "TextField", "props": {}},
+                                   "foo": {"type": "TextField", "props": {}}}, {}, "id")
+    schema: aiohttp_admin.Schema = {"security": {"check_credentials": check_credentials},
+                                    "resources": ({"model": model, "display": ("bar",)},)}
+
+    with pytest.raises(ValueError, match=r"Display includes non-existent field \('bar',\)"):
+        admin = aiohttp_admin.setup(app, schema)
