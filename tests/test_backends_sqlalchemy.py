e6e3db73febc933e688c56f14d5def88dd994c2c
Sam Bull <git@sambull.org>
2023-10-02 17:40:41 +0100

    Add or to tests

--- tests/test_backends_sqlalchemy.py
+++ tests/test_backends_sqlalchemy.py
@@ -276,14 +276,16 @@ def test_check_constraints(base: type[DeclarativeBase], mock_engine: AsyncEngine
         min_length: Mapped[str] = mapped_column()
         min_length_gt: Mapped[str] = mapped_column()
         regex: Mapped[str] = mapped_column()
-        with_and: Mapped[str] = mapped_column()
+        with_and: Mapped[int] = mapped_column()
+        with_or: Mapped[int] = mapped_column()
 
         __table_args__ = (sa.CheckConstraint(gt > 3), sa.CheckConstraint(gte >= 3),
                           sa.CheckConstraint(lt < 3), sa.CheckConstraint(lte <= 3),
                           sa.CheckConstraint(sa.func.char_length(min_length) >= 5),
                           sa.CheckConstraint(sa.func.char_length(min_length_gt) > 5),
                           sa.CheckConstraint(sa.func.regexp(regex, r"abc.*")),
-                          sa.CheckConstraint(sa.and_(with_and > 7, with_and < 12)))
+                          sa.CheckConstraint(sa.and_(with_and > 7, with_and < 12)),
+                          sa.CheckConstraint(sa.or_(with_and > 7, with_and < 12)))
 
     r = SAResource(mock_engine, TestCC)
 
@@ -304,6 +306,7 @@ def test_check_constraints(base: type[DeclarativeBase], mock_engine: AsyncEngine
     assert f["regex"]["props"]["validate"] == [required, func("regex", (regex("abc.*"),))]
     assert f["with_and"]["props"]["validate"] == [
         required, func("minValue", (8,)), func("maxValue", (11,))]
+    assert f["with_or"]["props"]["validate"] == [required]
 
 
 async def test_nonid_pk(base: type[DeclarativeBase], mock_engine: AsyncEngine) -> None:
