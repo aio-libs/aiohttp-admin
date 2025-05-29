ae2e3785c782f0e67190d8ad87d5f28d939e8aaf
Sam Bull <git@sambull.org>
2025-05-29 22:22:13 +0100

    Update vite.config.js

--- admin-js/vite.config.js
+++ admin-js/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    server: {
-        cors: {
-            // the origin you will be accessing via browser
-            origin: "http://localhost",
-        },
-    },
     build: {
         outDir: "../aiohttp_admin/static/",
         rollupOptions: {
