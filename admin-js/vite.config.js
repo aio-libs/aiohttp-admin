019bef62714f03e5ed50b0b1aac993ee35c42bf9
Sam Bull <git@sambull.org>
2025-05-29 22:23:15 +0100

    Update vite.config.js

--- admin-js/vite.config.js
+++ admin-js/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     build: {
+        minify: "terser",
         outDir: "../aiohttp_admin/static/",
         rollupOptions: {
             input: "src/admin.jsx",
