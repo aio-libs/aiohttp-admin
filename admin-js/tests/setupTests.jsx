025e0b09d80d78858eb76cd4c7e4178e4aec7cb2
Sam Bull <git@sambull.org>
2025-06-07 01:58:24 +0100

    Update setupTests.jsx

--- admin-js/tests/setupTests.jsx
+++ admin-js/tests/setupTests.jsx
@@ -99,6 +99,7 @@ let login = {"username": "admin", "password": "admin"};
 global.setLogin = (username, password) => { login = {username, password}; };
 
 beforeEach(async () => {
+    delete location.href;
     location.href = "http://localhost:8080/";
     localStorage.clear();
 
